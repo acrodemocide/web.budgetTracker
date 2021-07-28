7e7927deace41598264f63b4bfd0f6565afb8f3c
Daniel Howard <acrodemocide@hotmail.com>
2021-07-28 11:25:03 -0600

    Built basic toolbar and homepage

--- web.budget-tracker/src/App.tsx
+++ web.budget-tracker/src/App.tsx
@@ -1,26 +1,37 @@
+import React from 'react';
 import { AppBar, Button, IconButton, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import ToolBar from '@material-ui/core/Toolbar';
-import React from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core';
 
 function App() {
+  const classes: any = useStyles();
   return (
     <>
       <AppBar position="static">
         <ToolBar>
-          <IconButton edge="start">
+          <IconButton edge="start" className={classes.menuButton}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6">
             Budget Tracker
           </Typography>
-          <Button>Sign In</Button>
-          <Button>Sign Up</Button>
+          <Button className={classes.link}>Sign In</Button>
+          <Button className={classes.link}>Sign Up</Button>
         </ToolBar>
       </AppBar>
     </>
   );
 }
+
+const useStyles = makeStyles((theme: any): any => ({
+  menuButton: {
+    color: 'white',
+  },
+  link: {
+    color: 'white',
+  },
+}));
 
 export default App;
